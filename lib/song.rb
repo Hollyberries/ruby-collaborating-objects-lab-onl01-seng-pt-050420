25efb87878eda72e439274e50c5ac6570dbaf40a
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-06 21:47:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,10 @@ class Song
   end
   
   def self.new_by_filename
-    
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
   
   
